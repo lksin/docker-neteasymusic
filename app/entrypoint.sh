947ee61eb03ad4c18a49149107ed26c28fb7b5a9
lksin <64049501+lksin@users.noreply.github.com>
2022-03-28 17:54:01 +0800

    Update entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,7 +4,7 @@ sed -i 's/1234567/$QQ/' /usr/src/app/QQMusicApi/bin/config.js
 cd /usr/src/app/NeteaseCloudMusicApi && npm run start &
 sleep 5
 
-cd /usr/src/app/QQMusicApi && npm run start &
+cd /usr/src/app/QQMusicApi && yarn&yarn start &
 sleep 5
 
 exec "$@"
